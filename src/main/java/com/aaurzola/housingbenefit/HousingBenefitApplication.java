4905e4eeabec570c05676883ae9d55d35bc86d65
Alejandro Urzola <alejandrourzola@192.168.1.9>
2023-11-11 15:34:58 -0500

    first commit

---
+++ src/main/java/com/aaurzola/housingbenefit/HousingBenefitApplication.java
@@ -0,0 +1,13 @@
+package com.aaurzola.housingbenefit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingBenefitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousingBenefitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aaurzola/housingbenefit/HousingBenefitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaurzola.housingbenefit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousingBenefitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
