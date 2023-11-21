7153b3fd9dc4ebc5dff0a91689ac54611d7438f6
Alejandro Urzola <aaurzola@gmail.com>
2023-11-21 09:59:39 -0500

    typo(EmptyRequesterException): add missing period

--- src/main/java/com/aaurzola/housingbenefit/exception/EmptyRequesterIdException.java
+++ src/main/java/com/aaurzola/housingbenefit/exception/EmptyRequesterIdException.java
@@ -3,6 +3,6 @@ package com.aaurzola.housingbenefit.exception;
 public class EmptyRequesterIdException extends RuntimeException {
 
     public EmptyRequesterIdException() {
-        super("La lista de solicitantes no puede estar vacía");
+        super("La lista de solicitantes no puede estar vacía.");
     }
 }
