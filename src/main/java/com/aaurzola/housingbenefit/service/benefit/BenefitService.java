3e42399e124b624d1d806c4313ea8e989584d6ee
Alejandro Urzola <aaurzola@gmail.com>
2023-11-22 23:22:53 -0500

    feat(benefit): add javadoc comments on service interface methods

--- src/main/java/com/aaurzola/housingbenefit/service/benefit/BenefitService.java
+++ src/main/java/com/aaurzola/housingbenefit/service/benefit/BenefitService.java
@@ -6,5 +6,10 @@ import java.util.List;
 
 public interface BenefitService {
 
+    /**
+     * finds all the details associated to a request
+     * @param requestId
+     * @return a list of entity 'Benefit'
+     */
     List<Benefit> findBenefitDetails(Long requestId);
 }
