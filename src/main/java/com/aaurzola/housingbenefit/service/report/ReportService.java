6a20d45731dda22611289c5b2941e4d214aa900b
Alejandro Urzola <aaurzola@gmail.com>
2023-11-22 23:22:05 -0500

    feat(report): add javadoc comments on service interface methods

--- src/main/java/com/aaurzola/housingbenefit/service/report/ReportService.java
+++ src/main/java/com/aaurzola/housingbenefit/service/report/ReportService.java
@@ -6,5 +6,12 @@ import java.io.IOException;
 
 
 public interface ReportService {
+
+    /**
+     * creates an Excel file with the information of a query result set from the 'Request' entity
+     * @param reportParams
+     * @return a downloadable file that contains the informacion of an .xlsx file
+     * @throws IOException
+     */
     byte[] createExcelWorkbook(ReportDTO reportParams) throws IOException;
 }
